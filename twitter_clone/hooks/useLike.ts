9ed7990bc2eae24659c8e30bb50810486ba3f222
Lukebot19 <lukealexander2a@gmail.com>
2023-04-12 17:39:25 +0100

    Likes

--- twitter_clone/hooks/useLike.ts
+++ twitter_clone/hooks/useLike.ts
@@ -1,54 +1,54 @@
+import axios from "axios";
 import { useCallback, useMemo } from "react";
-import useCurrentUser from "./useCurrentUser"
+import { toast } from "react-hot-toast";
+
+import useCurrentUser from "./useCurrentUser";
 import useLoginModal from "./useLoginModal";
 import usePost from "./usePost";
 import usePosts from "./usePosts";
-import { toast } from "react-hot-toast";
-import axios from "axios";
 
-const useLike = ({postId, userId}: {postId:string, userId?: string}) => {
-    const { data: currentUser } = useCurrentUser();
+const useLike = ({ postId, userId }: { postId: string, userId?: string }) => {
+  const { data: currentUser } = useCurrentUser();
+  const { data: fetchedPost, mutate: mutateFetchedPost } = usePost(postId);
+  const { mutate: mutateFetchedPosts } = usePosts(userId);
 
-    const { data: fetchedPost, mutate:mutateFetchedPost } = usePost(postId);
-    
-    const { mutate: mutateFetchedPosts } = usePosts(userId);
+  const loginModal = useLoginModal();
 
-    const loginModal = useLoginModal();
+  const hasLiked = useMemo(() => {
+    const list = fetchedPost?.likedIds || [];
 
-    const hasLiked = useMemo(() => {
-        const List = fetchedPost?.likedIds || [];
+    return list.includes(currentUser?.id);
+  }, [fetchedPost, currentUser]);
 
-        return List.includes(currentUser?.id);
-    }, [currentUser?.id, fetchedPost?.likedIds]);
-
-    const toggleLike = useCallback( async () => {
-        if (!currentUser) {
-            return loginModal.onOpen();
-        }
-
-        try {
-            let request;
-
-            if (hasLiked) {
-                request = () => axios.delete('/api/like', {data: {postId}});
-            } else {
-                request = () => axios.post('/api/like', {postId});
-            }
-
-            await request();
-            mutateFetchedPost();
-            mutateFetchedPosts();
-
-            toast.success("Success");
-        } catch (error) {
-            toast.error("Something went wrong");
-        }
-    }, [currentUser, hasLiked, postId, mutateFetchedPost,mutateFetchedPosts, loginModal]);
-
-    return {
-        hasLiked,
-        toggleLike
+  const toggleLike = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
     }
-};
+
+    try {
+      let request;
+
+      if (hasLiked) {
+        request = () => axios.delete('/api/like', { data: { postId } });
+      } else {
+        request = () => axios.post('/api/like', { postId });
+      }
+
+      await request();
+      mutateFetchedPost();
+      mutateFetchedPosts();
+
+      toast.success('Success');
+    } catch (error) {
+        console.log(error);
+      toast.error('Something went wrong');
+    }
+  }, [currentUser, hasLiked, postId, mutateFetchedPosts, mutateFetchedPost, loginModal]);
+
+  return {
+    hasLiked,
+    toggleLike,
+  }
+}
 
 export default useLike;
