5e2501160c678f47ebf636c62a0ddc29d8d2586d
Lukebot19 <lukealexander2a@gmail.com>
2023-04-13 13:52:10 +0100

    Comments

--- twitter_clone/components/Form.tsx
+++ twitter_clone/components/Form.tsx
@@ -7,6 +7,7 @@ import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
 import Button from "./Button";
 import Avatar from "./Avatar";
+import usePost from "@/hooks/usePost";
 
 interface FormProps {
     placeholder: string;
@@ -21,6 +22,7 @@ const Form: React.FC<FormProps> = ({placeholder, isComment, postId}) => {
 
     const { data:currentUser } = useCurrentUser();
     const { mutate: mutatePosts } = usePosts();
+    const { mutate: mutatePost } = usePost(postId as string);
 
     const [body, setBody] = useState('');
     const [isLoading, setIsLoading] = useState(false);
@@ -29,7 +31,9 @@ const Form: React.FC<FormProps> = ({placeholder, isComment, postId}) => {
         try {
             setIsLoading(true);
 
-            await axios.post('/api/posts', {
+            const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts'
+
+            await axios.post(url, {
                 body
             });
 
@@ -38,13 +42,14 @@ const Form: React.FC<FormProps> = ({placeholder, isComment, postId}) => {
             setBody('');
 
             mutatePosts();
+            mutatePost();
 
         } catch (error) {
             toast.error("Something went wrong");
         } finally {
             setIsLoading(false);
         }
-    }, [body, mutatePosts]);
+    }, [body, mutatePosts, mutatePost, isComment, postId]);
 
     return (
         <div className="border-b-[1px] border-neutral-800 px-5 py-2">
